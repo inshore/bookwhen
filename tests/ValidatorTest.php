44ede6eccb853134989514c54c0b819903aaac28
Dan Mullin <email@danielmullin.com>
2020-08-09 14:37:34 +0200

    added tests and update id calls

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -14,14 +14,58 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
         $this->validator = new Validator();
     }
     
+    public function provideInvalidDates(): array
+    {
+        return [];
+    }
+    
+    public function provideInvalidFroms(): array
+    {
+        return [];
+    }
+    
+    public function provideInvalidIds(): array
+    {
+        return [];
+    }
+    
     public function provideInvalidTags(): array
     {
         return [
             'null' => [null],
             'emptyString' => [''],
-            'object' => [ new \stdClass() ], 
+            'object' => [ new \stdClass() ],
             'nonAlphanumeric'  => [ 'akrwejh[?fs83z420qrzc8397r4' ],
         ];
+    }
+    
+    public function provideInvalidTokens(): array
+    {
+        return [];
+    }
+    
+    public function provideInvalidTos(): array
+    {
+        return [];
+    }
+    
+    public function provideValidDates(): array
+    {
+        return [
+            'datePast' => [ '20191230' ],
+            'datePresent' => [ '20200809' ],
+            'dateFuture' => [ '20211230' ],
+        ];
+    }
+    
+    public function provideValidFroms(): array
+    {
+        return [];
+    }
+    
+    public function provideValidIds(): array
+    {
+        return [];
     }
     
     public function provideValidTags():array
@@ -37,6 +81,16 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
         ];
     }
     
+    public function provideValidTokens(): array
+    {
+        return [];
+    }
+    
+    public function provideValidTos(): array
+    {
+        return [];
+    }
+    
     /**
      * @dataProvider provideInvalidTags
      */
@@ -46,11 +100,49 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
+     * @dataProvider provideValidDates
+     */
+    public function testValidDateReturnsTrueOnValidDates($date)
+    {
+        $this->assertTrue($this->validator->validDate($date), $date);
+    }
+    /**
+     * @dataProvider provideValidFroms
+     */
+    public function testValidFromReturnsTrueOnValidFroms($tag)
+    {
+        $this->assertTrue($this->validator->validTag($tag), $tag);
+    }
+    /**
+     * @dataProvider provideValidIds
+     */
+    public function testValidIdReturnsTrueOnValidIds($id)
+    {
+        $this->assertTrue($this->validator->validId($id), $id);
+    }
+    
+    /**
      * @dataProvider provideValidTags
      */
     public function testValidTagReturnsTrueOnValidTags($tag)
     {
         $this->assertTrue($this->validator->validTag($tag), $tag);
+    }
+    
+    /**
+     * @dataProvider provideValidTokens
+     */
+    public function testValidTokenReturnsTrueOnValidTokens($token)
+    {
+        $this->assertTrue($this->validator->validToken($token), $token);
+    }
+    
+    /**
+     * @dataProvider provideValidTos
+     */
+    public function testValidTosReturnsTrueOnValidTos($to)
+    {
+        $this->assertTrue($this->validator->validTo($to), $to);
     }
 }
 

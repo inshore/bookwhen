b0d9dd00e23a19c37b5353173b2d4bfe7e3b50fa
Daniel Mullin <email@danielmullin.com>
2023-04-20 11:36:11 +0200

    Response

--- src/Responses/Events/RetrieveResponse.php
+++ src/Responses/Events/RetrieveResponse.php
@@ -37,8 +37,6 @@ final class RetrieveResponse implements ResponseContract
      */
     public static function from(array $attributes): self
     {
-        var_export($attributes);
-        die('here');
         return new self(
             $attributes['id'],
             $attributes['type'],

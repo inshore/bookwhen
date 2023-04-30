6310cc6900f72b405b9822c89f09336a9d521cc4
Daniel Mullin <email@danielmullin.com>
2023-04-30 18:36:11 +0200

    WIP

--- src/Contracts/ResponseContract.php
+++ src/Contracts/ResponseContract.php
@@ -15,31 +15,5 @@ use ArrayAccess;
  */
 interface ResponseContract extends ArrayAccess
 {
-    /**
-     * @param  key-of<TArray>  $offset
-     */
-    public function offsetExists(mixed $offset): bool;
 
-    /**
-     * @template TOffsetKey of key-of<TArray>
-     *
-     * @param  TOffsetKey  $offset
-     * @return TArray[TOffsetKey]
-     */
-    public function offsetGet(mixed $offset): mixed;
-
-    /**
-     * @template TOffsetKey of key-of<TArray>
-     *
-     * @param  TOffsetKey  $offset
-     * @param  TArray[TOffsetKey] $value
-     */
-    public function offsetSet(mixed $offset, mixed $value): never;
-
-    /**
-     * @template TOffsetKey of key-of<TArray>
-     *
-     * @param  TOffsetKey  $offset
-     */
-    public function offsetUnset(mixed $offset): never;
 }
